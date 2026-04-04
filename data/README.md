7d2f206c75e4ec6dcc35ff2af17e2a1795f9b710
Joel Lum <joellumzh@gmail.com>
2026-04-05 02:07:39 +1000

    added data retrieval for news summary

--- data/README.md
+++ data/README.md
@@ -1,10 +1,10 @@
 # Data
 
-All data files are ignored and must be downloaded (through scripts provided).
+All data files are ignored and must be downloaded (scripts provided).
 
 ---
 
-# Folder Structure
+## Folder Structure
 
 ```text
 data
@@ -21,15 +21,27 @@ data
 
 ---
 
-## 1. Financial PhraseBank
+## News Sentiment
+
+### 1. Financial PhraseBank
 
 **Source:** https://www.kaggle.com/datasets/ankurzing/sentiment-analysis-for-financial-news
-
----
 
 ## 2. *(Second dataset TBD)*
 
 **Source:** *(link)*
+
+---
+
+## News Summary
+
+### 1. News Summary
+
+**Source:** https://www.kaggle.com/datasets/sunnysai12345/news-summary
+
+### 2. CNN-DailyMail News Text Summarisation
+
+**Source:** https://www.kaggle.com/datasets/gowrishankarp/newspaper-text-summarization-cnn-dailymail
 
 ---
 
--- data/retrieve.py
+++ data/retrieve.py
@@ -18,7 +18,7 @@ os.makedirs(RAW, exist_ok=True)
 
 SENTIMENT = os.path.join(RAW, "sentiment")
 os.makedirs(SENTIMENT, exist_ok=True)
-SUMMARY = os.path.join(RAW, "sentiment")
+SUMMARY = os.path.join(RAW, "summary")
 os.makedirs(SUMMARY, exist_ok=True)
 
 
@@ -40,6 +40,7 @@ def kaggle(dataset, dest):
     print(f"[Kaggle] Done: {dataset}")
 
 
+# --- news sentiment
 # dataset 1: financial phrasebank
 def download_fpb():
     FPB = os.path.join(SENTIMENT, "FPB")
@@ -54,12 +55,40 @@ def download_fpb():
         os.rename(src, dst)
 
 
+# --- news summary
+# dataset 1: news summary
+def download_ns():
+    NS = os.path.join(SUMMARY, "NS")
+    os.makedirs(NS, exist_ok=True)
+
+    kaggle("sunnysai12345/news-summary", NS)
+
+    # standardised name
+    src = os.path.join(NS, "news_summary.csv")
+    dst = os.path.join(NS, "raw.csv")
+    if os.path.exists(src):
+        os.rename(src, dst)
+
+# dataset 2: CNN-DailyMail News Text Summarisation
+def download_CNN_DM():
+    CNN_DM = os.path.join(SUMMARY, "CNN_DM")
+    os.makedirs(CNN_DM, exist_ok=True)
+
+    kaggle("gowrishankarp/newspaper-text-summarization-cnn-dailymail", CNN_DM)
+
+
 # ----- main ---------------------
 
 if __name__ == "__main__":
-    print("Downloading datasets...\n")
+    print("Downloading datasets...")
 
     requirements()
+
+    print("\n\nRetrieving news sentiment datasets...")
     download_fpb()
+
+    print("\n\nRetrieving news summary datasets...")
+    download_ns()
+    download_CNN_DM()
 
     print("\n\nDatasets successfully downloaded.")
